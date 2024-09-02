ca48a6c8828f5482a128744b0f84cf8c0d86645e
Yu Wu <wugo@amazon.co.jp>
2024-09-02 06:43:33 +0000

    response markdown adaptation

---
+++ react/my-app/src/components/MarkdownRenderer.js
@@ -0,0 +1,34 @@
+import ReactMarkdown from 'react-markdown';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { vscDarkPlus } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import './MarkdownRenderer.css';
+
+function MarkdownRenderer({ content }) {
+  const components = {
+    code({ node, inline, className, children, ...props }) {
+      const match = /language-(\w+)/.exec(className || '');
+      return !inline && match ? (
+        <SyntaxHighlighter
+          style={vscDarkPlus}
+          language={match[1]}
+          PreTag="div"
+          {...props}
+        >
+          {String(children).replace(/\n$/, '')}
+        </SyntaxHighlighter>
+      ) : (
+        <code className={className} {...props}>
+          {children}
+        </code>
+      );
+    },
+  };
+
+  return (
+    <div className="markdown-content">
+      <ReactMarkdown components={components}>{content}</ReactMarkdown>
+    </div>
+  );
+}
+
+export default MarkdownRenderer;
